90c5f987058a1335ac233494027d1fd8246d970b
frankpulido <frankpulido@me.com>
2024-10-17 17:45:56 +0200

    2 validations: turns count and number of winning lines

--- index.php
+++ index.php
@@ -2,15 +2,40 @@
 declare(strict_types = 1);
 require "tic_tac_toe.php";
 
-$array1 = [['X','O','O'],['O','X','O'],['-','O','X']];
+/*
+README Rubén
+Cambios hechos para validar resultados :
+
+1) Invalidar de existir más de 1 ganador.
+Los bucles do-while de los métodos checkRows y checkColumns :
+    La condición era : while ($i <= 2 || $end = false)
+    La he cambiado por si acaso existían 2 columnas iguales : while ($i <= 2)
+Antes se interrumpía el bucle al encontrar el primer "3 en raya"" (fila o columna), ahora podría encontrar más de una raya horizontal o vertical.
+$witness podría contar 8 rayas de llenar el tablero totalmente con el miemo símbolo.
+
+Me vi tentado a hacer un merge de los 3 métodos de verificación de filas/columnas/diagonales, pero pensando en posible evolución del ejercicio dando información más detallada creí conveniente dejar de momento los 3 de forma separada.
+
+2) Invalidar de haber ocurrido un salto en el turno de jugada.
+Esta verificación la haré antes.
+La diferencia de turnos de jugada no puede ser de más de 1 turno.
+
+3) Tamaño del array recibido y cantidad de símbolos usados.
+Se sobreentiende que esto es problema del diseño de captura de datos. NO VALIDO.
+*/
+
+$array1 = [['X','O','O'],['O','X','O'],['-','O','X']]; // Array Rubén : INVALIDADO (X juega 3 veces y O juega 5)
 $array2 = [['O','X','-'],['O','X','-'],['O','-','-']];
 $array3 = [['O','X','O'],['X','O','X'],['X','X','O']];
 $array4 = [['X','X','O'],['O','O','X'],['X','O','O']];
+$array5 = [['X','X','X'],['O','O','O'],['X','X','O']]; // INVALIDADO (2 ganadores horizontales)
+$array6 = [['O','X','O'],['O','X','X'],['O','X','X']]; // INVALIDADO (2 ganadores verticales)
 
 $game1 = new TicTacToe($array1);
 $game2 = new TicTacToe($array2);
 $game3 = new TicTacToe($array3);
 $game4 = new TicTacToe($array4);
+$game5 = new TicTacToe($array5);
+$game6 = new TicTacToe($array6);
 
 echo PHP_EOL;
 $winner1 = $game1->checkWinner();
@@ -24,5 +49,12 @@ echo $winner3;
 echo PHP_EOL;
 $winner4 = $game4->checkWinner();
 echo $winner4;
+echo PHP_EOL;
+$winner5 = $game5->checkWinner();
+echo $winner5;
+echo PHP_EOL;
+$winner6 = $game6->checkWinner();
+echo $winner6;
+echo PHP_EOL;
 echo PHP_EOL;
 ?>
--- tic_tac_toe.php
+++ tic_tac_toe.php
@@ -1,8 +1,9 @@
 <?php
-// kata tic-tac-toe
+declare(strict_types=1);
+
 class TicTacToe {
     // Atributos
-    protected array $input = [];
+    protected array $input;
     private int $witness;
 
     public function __construct($input, $witness = 0)
@@ -10,6 +11,16 @@ class TicTacToe {
         $this->input = $input;
         $this->witness = $witness;
     }
+
+    public function validateTurnCount() : bool {
+        $passed = false;
+        $count_turns = array_count_values(array_merge($this->input[0],$this->input[1],$this->input[2]));
+        $turnsX = array_key_exists("X", $count_turns)? $count_turns["X"] : 0;
+        $turnsO = array_key_exists("O", $count_turns)? $count_turns["O"] : 0;
+        if(abs($turnsX - $turnsO) < 2){$passed = true;}
+        return $passed;
+    }
+
     public function checkRows() : string {
         $reply = "The winner is : ";
         $end = false;
@@ -21,9 +32,10 @@ class TicTacToe {
                 $this->witness++;
             }
             $i++;
-        } while ($i <= 2 || $end = false);
+        } while ($i <= 2);
         return $reply;
     }
+
     public function checkColumns() : string {
         $reply = "The winner is : ";
         $end = false;
@@ -35,9 +47,10 @@ class TicTacToe {
                 $this->witness++;
             }
             $i++;
-        } while ($i <= 2 || $end = false);
+        } while ($i <= 2);
         return $reply;
     }
+
     public function checkDiagonals() : string {
         $reply = "The winner is : ";
         if(($this->input[0][0] == $this->input[1][1] && $this->input[1][1] == $this->input[2][2]) && ($this->input[2][2] != "-")){
@@ -50,18 +63,22 @@ class TicTacToe {
         }
         return $reply;
     }
+
     public function checkWinner() : string {
         $reply = "";
-        $compare = "The winner is : ";
-        $reply1 = $this->checkRows();
-        $reply2 = $this->checkColumns();
-        $reply3 = $this->checkDiagonals();
-        if($this->witness > 1){$reply = "Invalid game!!!";}
-        elseif(($reply1 == $reply2) && ($reply2 == $reply3) && ($reply1 == $compare)){$reply = "No winner, truce!!";}
-        elseif(!($reply1 == $compare)){$reply = $reply1;}
-        elseif(!($reply2 == $compare)){$reply = $reply2;}
-        else{$reply = $reply3;}
-        return $reply;
+        if($this->validateTurnCount()){
+            $compare = "The winner is : ";
+            $reply1 = $this->checkRows();
+            $reply2 = $this->checkColumns();
+            $reply3 = $this->checkDiagonals();
+            if($this->witness > 1){$reply = "Invalid game!!!. We found $this->witness lines (with 3 matching symbols) in total.";}
+            elseif(($reply1 == $reply2) && ($reply2 == $reply3) && ($reply1 == $compare)){$reply = "No winner, truce!!";}
+            elseif(!($reply1 == $compare)){$reply = $reply1;}
+            elseif(!($reply2 == $compare)){$reply = $reply2;}
+            else{$reply = $reply3;}
+        }
+        else{$reply = "Game doesn't pass plays turn count";}
+        return $reply;    
     }
 }
 ?>
